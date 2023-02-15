bfeb57bb997e4df2f0ff77f8cca4ffd66333e5f4
Keshav Sharma <skeshav0825@gmail.com>
2023-02-15 20:36:41 +0530

    fix: updated hook - getCurrentImageURI

--- src/hooks/useFetchImageURI.ts
+++ src/hooks/useFetchImageURI.ts
@@ -3,23 +3,16 @@ import config from '../utils/vyktContract.json';
 import makeBlockie from 'ethereum-blockies-base64';
 
 export const useFetchImageURI = (userAddress: string) => {
-  const { data, error, isLoading } = useContractRead({
+  const { data, refetch } = useContractRead({
     address: config.address as Address,
     abi: config.abi,
-    functionName: 'getImageURIs',
+    functionName: 'getCurrentImageURI',
     args: [userAddress],
   });
 
-  const imageURIs = data as string[];
-
-  if (imageURIs && imageURIs.length > 0) {
-    console.log('imageURIs', imageURIs);
-    return { data: imageURIs[imageURIs.length - 1] };
+  if (data === '') {
+    return { data: makeBlockie(userAddress), refetch };
   } else {
-    const blockiesURI = makeBlockie(userAddress);
-    console.log('blockiesURI', blockiesURI);
-    return { data: blockiesURI };
+    return { data, refetch };
   }
-
-  return { data, error, isLoading };
 };
