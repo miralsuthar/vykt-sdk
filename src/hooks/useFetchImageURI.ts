7b7878d63039b98f4466896422a88e81260fa0c7
Keshav Sharma <skeshav0825@gmail.com>
2023-02-17 17:50:35 +0530

    fix: increased size-limit & deleted example folder

--- src/hooks/useFetchImageURI.ts
+++ src/hooks/useFetchImageURI.ts
@@ -7,27 +7,23 @@ export const useFetchImageURI = (address: string) => {
   const [data, setData] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    async function getProfile() {
-      setIsLoading(true);
-      const provider = new ethers.providers.JsonRpcProvider(config.rpc);
-      const contract = new ethers.Contract(
-        config.address,
-        config.abi,
-        provider
-      );
+  async function getProfile() {
+    setIsLoading(true);
+    const provider = new ethers.providers.JsonRpcProvider(config.rpc);
+    const contract = new ethers.Contract(config.address, config.abi, provider);
 
-      const data = await contract.getCurrentImageURI(address);
+    const data = await contract.getCurrentImageURI(address);
 
-      if (data === '') {
-        setData(makeBlockie(address));
-        setIsLoading(false);
-        return;
-      }
-      setData(data);
+    if (data === '') {
+      setData(makeBlockie(address));
       setIsLoading(false);
+      return;
     }
+    setData(data);
+    setIsLoading(false);
+  }
 
+  useEffect(() => {
     getProfile();
   }, [address]);
 
